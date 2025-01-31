870c71871f6877a99f04037b88a88cb9bf616b9f
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2025-01-31 09:07:01 +0100

    Update index.py

--- index.py
+++ index.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 # Título
 st.title("Kentu Ventas 2025")
+# Descripción
+st.write("""
+Este informe presenta las predicciones de ventas de la empresa Kentu para el año 2025. 
+A través de análisis y proyecciones, se anticipa el desempeño y las tendencias del mercado que impactarán en los resultados de la compañía.
+""")
